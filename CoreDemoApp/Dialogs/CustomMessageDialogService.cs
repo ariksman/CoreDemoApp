66a85d29e63208670d160681292b7ad3f7dc27e8
Antti Riksman <antti.riksman@gmail.com>
2019-09-19 22:43:55 +0300

    Create CustomMessageDialogService.cs

---
+++ CoreDemoApp/Dialogs/CustomMessageDialogService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+
+namespace CoreDemoApp.Dialogs
+{
+  public class CustomMessageDialogService : IMessageDialogService
+  {
+    public void ShowErrorMessage(string sender, string message, string details)
+    {
+      new InfoDialog(message, details)
+      {
+        WindowStartupLocation = WindowStartupLocation.CenterOwner,
+        Owner = App.Current.MainWindow
+      }.ShowDialog();
+    }
+
+    public void ShowWarningMessage(string sender, string message, string details)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void ShowUserMessage(string sender, string message, Exception ex)
+    {
+      throw new NotImplementedException();
+    }
+
+    public bool Confirm(string sender, string message, string details)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void ShowException(string sender, Exception ex)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
