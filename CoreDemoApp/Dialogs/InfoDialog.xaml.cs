072fcec9c290a96f94766121872344321d61ef70
Antti Riksman <antti.riksman@gmail.com>
2019-09-19 22:43:44 +0300

    Create InfoDialog.xaml.cs

---
+++ CoreDemoApp/Dialogs/InfoDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace CoreDemoApp.Dialogs
+{
+  /// <summary>
+  /// Interaction logic for InfoDialog.xaml
+  /// </summary>
+  public partial class InfoDialog : Window
+  {
+    public InfoDialog(string title, string message)
+    {
+      InitializeComponent();
+      Title = title;
+      textBlock.Text = message;
+    }
+
+    private void ButtonYes_Click(object sender, RoutedEventArgs e)
+    {
+      DialogResult = true;
+    }
+
+    private void ButtonNo_Click(object sender, RoutedEventArgs e)
+    {
+      DialogResult = false;
+    }
+  }
+}
