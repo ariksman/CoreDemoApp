2cf5a0d974720af6d8d5653194fb8e54751fb17c
Antti Riksman <antti.riksman@gmail.com>
2019-09-19 22:39:15 +0300

    Update IMessageDialogService.cs

--- CoreDemoApp.Application/Interfaces/IMessageDialogService.cs
+++ CoreDemoApp.Application/Interfaces/IMessageDialogService.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace CoreDemoApp.Application.Interfaces
 {
-  class Class1
+  public interface IMessageDialogService
   {
+    void ShowErrorMessage(string sender, string message, string details);
+    void ShowWarningMessage(string sender, string message, string details);
+    void ShowUserMessage(string sender, string message, Exception ex);
+    bool Confirm(string sender, string message, string details);
+    void ShowException(string sender, Exception ex);
   }
 }
