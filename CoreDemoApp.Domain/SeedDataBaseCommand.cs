2787c814b9e5c3507d8fe00d47d9260eb4c528cd
Antti Riksman <antti.riksman@gmail.com>
2019-09-19 21:21:33 +0300

    Create SeedDataBaseCommand.cs

---
+++ CoreDemoApp.Domain/SeedDataBaseCommand.cs
@@ -0,0 +1,16 @@
+﻿using CoreDemoApp.Common.CQS;
+
+namespace CoreDemoApp.Domain
+{
+  public class SeedDatabaseCommand : ICommand
+  {
+    public int Size { get; set; }
+    public string Connectionstring { get; set; }
+
+    public SeedDatabaseCommand(int size, string connectionstring = null)
+    {
+      Size = size;
+      Connectionstring = connectionstring;
+    }
+  }
+}
