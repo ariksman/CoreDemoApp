4375036e1759f79fd250d8cd8750b7d84567f365
Antti Riksman <antti.riksman@gmail.com>
2019-09-19 20:32:53 +0300

    Create Age.cs

---
+++ CoreDemoApp.Domain/Age.cs
@@ -0,0 +1,41 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace CoreDemoApp.Domain
+{
+  public class Age : ValueObject<Age>
+  {
+    public int Value { get; }
+
+    public Age(int value)
+    {
+      Value = value;
+    }
+
+    public static Result<Age> Create(Maybe<int> ageOrNothing)
+    {
+      return ageOrNothing.ToResult("Argument is null")
+        .Ensure(value => value >= 0, "Age must be a positive number")
+        .Map(value => new Age(value));
+    }
+
+    protected override bool EqualsCore(Age other)
+    {
+      return Value == other.Value;
+    }
+
+    protected override int GetHashCodeCore()
+    {
+      return Value.GetHashCode();
+    }
+
+    public static implicit operator int(Age age)
+    {
+      return age.Value;
+    }
+
+    public static explicit operator Age(int age)
+    {
+      return Create(age).Value;
+    }
+  }
+}
