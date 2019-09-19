2d5d604715d9e2025acf970abb464cd9b5bfeb7b
Antti Riksman <antti.riksman@gmail.com>
2019-09-19 22:43:49 +0300

    Create IMessageDialogService.cs

---
+++ CoreDemoApp/Dialogs/IMessageDialogService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoreDemoApp.Dialogs
+{
+  public interface IMessageDialogService
+  {
+    void ShowErrorMessage(string sender, string message, string details);
+    void ShowWarningMessage(string sender, string message, string details);
+    void ShowUserMessage(string sender, string message, Exception ex);
+    bool Confirm(string sender, string message, string details);
+    void ShowException(string sender, Exception ex);
+  }
+}
