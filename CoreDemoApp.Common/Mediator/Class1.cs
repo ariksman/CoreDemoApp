4cc47441a2ef17ef9ba87c85bde1d55d833986d1
Antti Riksman <antti.riksman@gmail.com>
2019-09-20 00:22:02 +0300

    Added MediatR

--- CoreDemoApp.Common/Mediator/Class1.cs
+++ CoreDemoApp.Common/Mediator/Class1.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace CoreDemoApp.Common.Mediator
 {
-  class Class1
+  public interface INotifierMediatorService
   {
+    void Notify(string notifyText);
+  }
+
+  public class NotifierMediatorService : INotifierMediatorService
+  {
+    private readonly IMediator _mediator;
+
+    public NotifierMediatorService(IMediator mediator)
+    {
+      _mediator = mediator;
+    }
+
+    public void Notify(string notifyText)
+    {
+      _mediator.Publish(new NotificationMessage { NotifyText = notifyText });
+    }
   }
 }
