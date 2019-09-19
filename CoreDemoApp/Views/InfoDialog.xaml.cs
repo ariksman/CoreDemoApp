efee3e58578a0a676412c359b2e99e7e4ce29e2d
Antti Riksman <antti.riksman@gmail.com>
2019-09-19 22:20:05 +0300

    new dialog

---
+++ CoreDemoApp/Views/InfoDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoreDemoApp.Views
+{
+  /// <summary>
+  /// Interaction logic for InfoDialog.xaml
+  /// </summary>
+  public partial class InfoDialog : Window
+  {
+    public InfoDialog()
+    {
+      InitializeComponent();
+    }
+  }
+}
