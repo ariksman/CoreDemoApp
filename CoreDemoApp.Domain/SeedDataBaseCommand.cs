c26f3db35f838f56b888b0c41252992ac2d21140
Antti Riksman <antti.riksman@gmail.com>
2019-09-19 21:57:47 +0300

    Update SeedDataBaseCommand.cs

--- CoreDemoApp.Domain/SeedDataBaseCommand.cs
+++ CoreDemoApp.Domain/SeedDataBaseCommand.cs
@@ -5,12 +5,12 @@ namespace CoreDemoApp.Domain
   public class SeedDatabaseCommand : ICommand
   {
     public int Size { get; set; }
-    public string Connectionstring { get; set; }
+    public string ConnectionString { get; set; }
 
-    public SeedDatabaseCommand(int size, string connectionstring = null)
+    public SeedDatabaseCommand(int size, string connectionString = null)
     {
       Size = size;
-      Connectionstring = connectionstring;
+      ConnectionString = connectionString;
     }
   }
 }
