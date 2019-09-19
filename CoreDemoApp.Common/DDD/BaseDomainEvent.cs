5c3604bb583074e23d4c9f1767a2c319ce6ddb12
Antti Riksman <antti.riksman@gmail.com>
2019-09-20 00:11:04 +0300

    Create BaseDomainEvent.cs

---
+++ CoreDemoApp.Common/DDD/BaseDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CoreDemoApp.Domain.DDD
+{
+  public abstract class BaseDomainEvent
+  {
+    public DateTime DateOccurred { get; protected set; } = DateTime.UtcNow;
+  }
+}
