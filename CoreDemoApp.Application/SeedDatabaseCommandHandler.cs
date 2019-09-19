d64201b0f13e33e097ef5062dadba7b732a038b5
Antti Riksman <antti.riksman@gmail.com>
2019-09-19 21:22:03 +0300

    Create SeedDatabaseCommandHandler.cs

---
+++ CoreDemoApp.Application/SeedDatabaseCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using CoreDemoApp.Common.CQS;
+using CoreDemoApp.Domain;
+using CSharpFunctionalExtensions;
+using Repository.Core;
+
+namespace CoreDemoApp.Application
+{
+  public class SeedDatabaseCommandHandler : ICommandHandler<SeedDatabaseCommand, Result>
+  {
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IDatabaseSeeder _databaseSeeder;
+
+    public SeedDatabaseCommandHandler(IUnitOfWork unitOfWork, IDatabaseSeeder databaseSeeder)
+    {
+      _unitOfWork = unitOfWork;
+      _databaseSeeder = databaseSeeder;
+    }
+
+    public Result Handle(SeedDatabaseCommand command)
+    {
+      try
+      {
+        _databaseSeeder.SeedDatabase(_unitOfWork);
+        _unitOfWork.Complete();
+        return Result.Ok();
+      }
+      catch (Exception e)
+      {
+        return Result.Fail(e.Message);
+      }
+    }
+
+    public Task<Result> HandleAsync(SeedDatabaseCommand command)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
