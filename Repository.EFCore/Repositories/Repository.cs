314283f2497886becd4c292b6ce11e04f5de4e98
Antti Riksman <antti.riksman@gmail.com>
2019-09-20 18:06:24 +0300

    Update Repository.cs

--- Repository.EFCore/Repositories/Repository.cs
+++ Repository.EFCore/Repositories/Repository.cs
@@ -1,63 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using Repository.Core.Repositories;
 
 namespace Repository.EFCore.Repositories
 {
-    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+  public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+  {
+    protected readonly DbContext Context;
+    internal DbSet<TEntity> DbSet;
+
+    protected Repository(DbContext context)
     {
-        protected readonly DbContext Context;
-
-        protected Repository(DbContext context)
-        {
-            Context = context;
-        }
-
-        public TEntity Get(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<TEntity> GetAll()
-        {
-            return Context.Set<TEntity>().AsNoTracking().ToList();
-        }
-
-        public void Add(TEntity entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void AddRange(IEnumerable<TEntity> entities)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Remove(TEntity entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveRange(IEnumerable<TEntity> entities)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Load()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Clear()
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Count()
-        {
-            throw new NotImplementedException();
-        }
+      Context = context;
+      DbSet = context.Set<TEntity>();
     }
+
+    public IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null,
+      Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+      string includeProperties = "")
+    {
+      IQueryable<TEntity> query = DbSet;
+      if (filter != null)
+      {
+        query = query.Where(filter);
+      }
+
+      query = includeProperties
+        .Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries)
+        .Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+
+      return orderBy != null ? orderBy(query).ToList() : query.ToList();
+    }
+
+    public TEntity GetById(int id)
+    {
+      return DbSet.Find(id);
+    }
+
+    public IEnumerable<TEntity> GetAll()
+    {
+      return DbSet.AsNoTracking().ToList();
+    }
+
+    public void Add(TEntity entity)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void AddRange(IEnumerable<TEntity> entities)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Remove(TEntity entity)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void RemoveRange(IEnumerable<TEntity> entities)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Load()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+      Context.RemoveRange(DbSet.ToList()); 
+    }
+
+    public int Count()
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
