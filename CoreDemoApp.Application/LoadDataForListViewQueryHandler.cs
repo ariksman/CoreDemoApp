1625ff30a3d2383c68e2c07c80e4f2ea7d5d0e85
Antti Riksman <antti.riksman@gmail.com>
2019-09-20 15:09:09 +0300

    Create LoadDataForListViewQueryHandler.cs

---
+++ CoreDemoApp.Application/LoadDataForListViewQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CoreDemoApp.Core.CQS;
+using CoreDemoApp.Domain;
+using CSharpFunctionalExtensions;
+using Repository.Core;
+using Repository.Core.DataModel;
+
+namespace CoreDemoApp.Application
+{
+  public class
+    LoadDataForListViewQueryHandler : IQueryHandler<LoadDataForListViewQuery, Result<List<Worker>>>
+  {
+    private readonly IUnitOfWork _unitOfWork;
+
+    public LoadDataForListViewQueryHandler(IUnitOfWork unitOfWork)
+    {
+      _unitOfWork = unitOfWork;
+    }
+
+    public Result<List<Worker>> Handle(LoadDataForListViewQuery query)
+    {
+      try
+      {
+        var data = _unitOfWork.Workers.GetAllWorkersWithEmployers();
+        return Result.Ok(data);
+      }
+      catch (Exception e)
+      {
+        return Result.Fail<List<Worker>>(e.Message);
+      }
+    }
+
+    public Task<Result<List<Worker>>> HandleAsync(LoadDataForListViewQuery query)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
