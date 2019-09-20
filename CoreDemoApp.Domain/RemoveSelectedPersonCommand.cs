409a5dc52f56751d558962100ee5ed0b2234305d
Antti Riksman <antti.riksman@gmail.com>
2019-09-20 18:05:58 +0300

    Create RemoveSelectedPersonCommand.cs

---
+++ CoreDemoApp.Domain/RemoveSelectedPersonCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CoreDemoApp.Core.CQS;
+using CSharpFunctionalExtensions;
+
+namespace CoreDemoApp.Domain
+{
+  public class RemoveSelectedPersonCommand : ICommand
+  {
+    public int? PersonId { get; set; }
+
+    public RemoveSelectedPersonCommand(int? personId)
+    {
+      PersonId = personId;
+    }
+  }
+}
