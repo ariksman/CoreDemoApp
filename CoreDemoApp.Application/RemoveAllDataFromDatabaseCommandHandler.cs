14be8c97e9dd818024fc246c5aa5dc7da1db51ae
Antti Riksman <antti.riksman@gmail.com>
2019-09-20 18:05:27 +0300

    Create RemoveAllDataFromDatabaseCommandHandler.cs

---
+++ CoreDemoApp.Application/RemoveAllDataFromDatabaseCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using CoreDemoApp.Core.CQS;
+using CoreDemoApp.Domain;
+using CoreDemoApp.Domain.ImpureServices;
+using CSharpFunctionalExtensions;
+using Repository.Core;
+
+namespace CoreDemoApp.Application
+{
+  public class RemoveAllDataFromDatabaseCommandHandler : ICommandHandler<RemoveAllDataFromDatabaseCommand, Result>
+  {
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RemoveAllDataFromDatabaseCommandHandler(IUnitOfWork unitOfWork)
+    {
+      _unitOfWork = unitOfWork;
+    }
+
+    public Result Handle(RemoveAllDataFromDatabaseCommand command)
+    {
+      try
+      {
+        _unitOfWork.Workers.Clear();
+        _unitOfWork.Employers.Clear();
+        _unitOfWork.Complete();
+        return Result.Ok();
+      }
+      catch (Exception e)
+      {
+        return Result.Fail(e.Message);
+      }
+    }
+
+    public Task<Result> HandleAsync(RemoveAllDataFromDatabaseCommand command)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
