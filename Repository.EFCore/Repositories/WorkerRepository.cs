638bc3861041a27904ab793eeb05282818e16b25
Antti Riksman <antti.riksman@gmail.com>
2019-09-20 18:06:28 +0300

    Update WorkerRepository.cs

--- Repository.EFCore/Repositories/WorkerRepository.cs
+++ Repository.EFCore/Repositories/WorkerRepository.cs
@@ -7,44 +7,45 @@ using Repository.Core.Repositories;
 
 namespace Repository.EFCore.Repositories
 {
-    public class WorkerRepository : Repository<Worker>, IWorkerRepository
+  public class WorkerRepository : Repository<Worker>, IWorkerRepository
+  {
+    public WorkerRepository(DbContext context) : base(context)
     {
-        public WorkerRepository(DbContext context) : base(context)
-        {
-        }
-
-        public bool AddWorker(Worker worker)
-        {
-                DatabaseContext.Workers.Add(worker);
-                return true;
-        }
-
-        public bool RemoveWorker(Worker worker)
-        {
-            throw new NotImplementedException();
-        }
-
-      public bool RemoveWorker(int workerId)
-      {
-        var localWorker = DatabaseContext.Workers.FirstOrDefault(w => w.WorkerId == workerId);
-        DatabaseContext.Workers.Remove(localWorker ?? throw new InvalidOperationException());
-        return true;
-      }
-
-      public Employer GetWorkerEmployer(Worker worker)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Worker> GetAllWorkersWithEmployers()
-        {
-            var workers = DatabaseContext.Workers
-                .Include(worker => worker.Employer1)
-                .ToList();
-
-            return workers;
-        }
-
-        public DatabaseContext DatabaseContext => Context as DatabaseContext;
     }
+
+    public bool AddWorker(Worker worker)
+    {
+      DatabaseContext.Workers.Add(worker);
+      return true;
+    }
+
+    public bool RemoveWorker(Worker worker)
+    {
+      DatabaseContext.Workers.Remove(worker ?? throw new InvalidOperationException());
+      return true;
+    }
+
+    public bool RemoveWorker(int workerId)
+    {
+      var localWorker = DatabaseContext.Workers.FirstOrDefault(w => w.WorkerId == workerId);
+      DatabaseContext.Workers.Remove(localWorker ?? throw new InvalidOperationException());
+      return true;
+    }
+
+    public Employer GetWorkerEmployer(Worker worker)
+    {
+      throw new NotImplementedException();
+    }
+
+    public List<Worker> GetAllWorkersWithEmployers()
+    {
+      var workers = DatabaseContext.Workers
+        .Include(worker => worker.Employer1)
+        .ToList();
+
+      return workers;
+    }
+
+    public DatabaseContext DatabaseContext => Context as DatabaseContext;
+  }
 }
