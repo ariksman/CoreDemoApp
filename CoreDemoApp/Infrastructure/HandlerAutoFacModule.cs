c6df377bf835fdafe164b5e749d663f973217ab8
Antti Riksman <antti.riksman@gmail.com>
2019-09-20 15:11:25 +0300

    Update HandlerAutoFacModule.cs

--- CoreDemoApp/Infrastructure/HandlerAutoFacModule.cs
+++ CoreDemoApp/Infrastructure/HandlerAutoFacModule.cs
@@ -2,10 +2,42 @@
 using System.Linq;
 using System.Reflection;
 using Autofac;
-using CoreDemoApp.Common.CQS;
+using CoreDemoApp.Core.CQS;
+using MediatR;
 
 namespace CoreDemoApp.Infrastructure
 {
+  public class MediatorModule : Autofac.Module
+  {
+    protected override void Load(ContainerBuilder builder)
+    {
+      builder.RegisterAssemblyTypes(typeof(IMediator).GetTypeInfo().Assembly)
+        .AsImplementedInterfaces();
+
+      // Register the DomainEventHandler classes (they implement INotificationHandler<>) in assembly holding the Domain Events
+      //builder.RegisterAssemblyTypes(typeof(ValidateOrAddBuyerAggregateWhenOrderStartedDomainEventHandler).GetTypeInfo().Assembly)
+      //  .AsClosedTypesOf(typeof(INotificationHandler<>));
+
+      // Register the Command's Validators (Validators based on FluentValidation library)
+      //builder
+      //  .RegisterAssemblyTypes(typeof(CreateOrderCommandValidator).GetTypeInfo().Assembly)
+      //  .Where(t => t.IsClosedTypeOf(typeof(IValidator<>)))
+      //  .AsImplementedInterfaces();
+
+
+      builder.Register<ServiceFactory>(context =>
+      {
+        var componentContext = context.Resolve<IComponentContext>();
+        return t => { object o; return componentContext.TryResolve(t, out o) ? o : null; };
+      });
+
+      //builder.RegisterGeneric(typeof(LoggingBehavior<,>)).As(typeof(IPipelineBehavior<,>));
+      //builder.RegisterGeneric(typeof(ValidatorBehavior<,>)).As(typeof(IPipelineBehavior<,>));
+      //builder.RegisterGeneric(typeof(TransactionBehaviour<,>)).As(typeof(IPipelineBehavior<,>));
+
+    }
+  }
+
   public class HandlerAutoFacModule : Autofac.Module
   {
 
