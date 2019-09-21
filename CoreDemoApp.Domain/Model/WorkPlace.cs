2050059983606ee04943f32590467182c1d2354f
Antti Riksman <antti.riksman@gmail.com>
2019-09-22 00:15:06 +0300

    Create WorkPlace.cs

---
+++ CoreDemoApp.Domain/Model/WorkPlace.cs
@@ -0,0 +1,13 @@
+﻿using CoreDemoApp.Domain.DDD;
+
+namespace CoreDemoApp.Domain.Model
+{
+  public class WorkPlace : Entity
+  {
+    public WorkPlace()
+    {
+    }
+
+    public string Name { get; set; }
+  }
+}
