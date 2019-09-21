5c34080779f2d47f3749f5c3e3288eedfa13024e
Antti Riksman <antti.riksman@gmail.com>
2019-09-22 00:14:44 +0300

    Create GetCurrentDatabaseConnectionQueryHandler.cs

---
+++ CoreDemoApp.Application/GetCurrentDatabaseConnectionQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using CoreDemoApp.Core.CQS;
+using CoreDemoApp.Domain;
+using CSharpFunctionalExtensions;
+using Repository.Core;
+
+namespace CoreDemoApp.Application
+{
+  public class
+    GetCurrentDatabaseConnectionQueryHandler : IQueryHandler<GetCurrentDatabaseConnectionQuery, Result<string>>
+  {
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetCurrentDatabaseConnectionQueryHandler(IUnitOfWork unitOfWork)
+    {
+      _unitOfWork = unitOfWork;
+    }
+
+    public Result<string> Handle(GetCurrentDatabaseConnectionQuery query)
+    {
+      try
+      {
+        var data = _unitOfWork.Connection;
+        return Result.Ok(data);
+      }
+      catch (Exception e)
+      {
+        return Result.Fail<string>(e.Message);
+      }
+    }
+
+    public Task<Result<string>> HandleAsync(GetCurrentDatabaseConnectionQuery query)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
