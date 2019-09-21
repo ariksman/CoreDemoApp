b9dd098aa1daa054137b04d2f6dfe8bd98583462
Antti Riksman <antti.riksman@gmail.com>
2019-09-22 00:14:48 +0300

    Create AddPersonWithEmployer.cs

---
+++ CoreDemoApp.Domain/AddPersonWithEmployer.cs
@@ -0,0 +1,16 @@
+﻿using CoreDemoApp.Core.CQS;
+using CoreDemoApp.Domain.Model;
+using Repository.Core.DataModel;
+
+namespace CoreDemoApp.Domain
+{
+  public class AddPersonWithEmployer : ICommand
+  {
+    public Worker Worker { get; }
+
+    public AddPersonWithEmployer(Worker worker)
+    {
+      Worker = worker;
+    }
+  }
+}
