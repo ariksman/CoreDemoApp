ca100a6821d60c6b81e08d422779512813646d3c
Antti Riksman <antti.riksman@gmail.com>
2019-09-22 00:16:09 +0300

    Update Worker.cs

--- Repository.Core/DataModel/Worker.cs
+++ Repository.Core/DataModel/Worker.cs
@@ -3,52 +3,11 @@ namespace Repository.Core.DataModel
 {
     public class Worker 
     {
-        public Worker()
-        {
-            
-        }
+        public Worker() {}
 
-        private int _workerId;
-        public int WorkerId
-        {
-            get { return _workerId; }
-            set
-            {
-                if (_workerId == value)
-                {
-                    return;
-                }
-                _workerId = value;
-            }
-        }
-
-        private string _name;
-        public string Name
-        {
-            get { return _name; }
-            set
-            {
-                if (_name == value)
-                {
-                    return;
-                }
-                _name = value;
-            }
-        }
-
-        private int _age;
-        public int Age
-        {
-            get { return _age; }
-            set
-            {
-                if (_age == value)
-                {
-                    return;
-                }
-                _age = value;
-            }
-        }
+        public int WorkerId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
 
         public virtual Employer Employer1 { get; set; }
     }
