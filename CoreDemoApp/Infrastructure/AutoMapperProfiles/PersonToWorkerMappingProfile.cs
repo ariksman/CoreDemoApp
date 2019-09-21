9b6ced9711b257790ad4da7bedb17ae657d1c5bf
Antti Riksman <antti.riksman@gmail.com>
2019-09-22 00:15:27 +0300

    Create PersonToWorkerMappingProfile.cs

---
+++ CoreDemoApp/Infrastructure/AutoMapperProfiles/PersonToWorkerMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CoreDemoApp.Domain.Model;
+using Repository.Core.DataModel;
+
+namespace CoreDemoApp.Infrastructure.AutoMapperProfiles
+{
+  public class PersonToWorkerMappingProfile : Profile
+  {
+    public PersonToWorkerMappingProfile()
+    {
+      CreateMap<Person, Worker>()
+        .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+        .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.Age))
+        .ReverseMap();
+    }
+  }
+}
