8064a003c0da3f8a6728c0235ebfc6b4d6e09152
Antti Riksman <antti.riksman@gmail.com>
2019-09-24 18:04:21 +0300

    Update PersonToPersonViewModelMappingProfile.cs

--- CoreDemoApp/Infrastructure/AutoMapperProfiles/PersonToPersonViewModelMappingProfile.cs
+++ CoreDemoApp/Infrastructure/AutoMapperProfiles/PersonToPersonViewModelMappingProfile.cs
@@ -11,6 +11,7 @@ namespace CoreDemoApp.Infrastructure.AutoMapperProfiles
       CreateMap<Person, PersonViewModel>()
         .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
         .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.Age))
+        .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
         .ReverseMap();
     }
   }
