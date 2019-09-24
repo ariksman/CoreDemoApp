e78c797a841db7928ed56917e86ccd637e6c3b02
Antti Riksman <antti.riksman@gmail.com>
2019-09-24 18:05:13 +0300

    using guids

--- Repository.Core/Repositories/IWorkerRepository.cs
+++ Repository.Core/Repositories/IWorkerRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Repository.Core.DataModel;
 
 namespace Repository.Core.Repositories
@@ -7,7 +8,7 @@ namespace Repository.Core.Repositories
     {
         bool AddWorker(Worker worker);
         bool RemoveWorker(Worker worker);
-        bool RemoveWorker(int workerId);
+        bool RemoveWorker(Guid workerId);
         Employer GetWorkerEmployer(Worker worker);
         List<Worker> GetAllWorkersWithEmployers();
     }
