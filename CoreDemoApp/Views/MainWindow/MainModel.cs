97ae07a99f31ea20c85ef2b1d1473f073843e3d0
Antti Riksman <antti.riksman@gmail.com>
2019-10-16 21:39:00 +0300

    Create MainModel.cs

---
+++ CoreDemoApp/Views/MainWindow/MainModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreDemoApp.Views.MainWindow
+{
+  public class MainModel
+  {
+    public string DatabaseConnectionPath { get; set; }
+    public int ItemCount { get; set; }
+    public bool IsChecked { get; set; }
+  }
+}
