a2212cfd7e3e80a062c78f6b550eda0f21b2356e
Antti Riksman <antti.riksman@gmail.com>
2019-10-16 21:28:02 +0300

    Create PersonModel.cs

---
+++ CoreDemoApp/Views/MainWindow/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoreDemoApp.Views.MainWindow
+{
+  public class PersonModel
+  {
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string WorkPlace { get; set; }
+    public int Age { get; set; }
+
+  }
+}
