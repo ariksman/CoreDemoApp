04190787a25ac2278c6101e6fa7ae2591d9341cc
Antti Riksman <antti.riksman@gmail.com>
2019-11-04 22:08:52 +0200

    Update DatabaseContext.cs

--- Repository.EFCore/DatabaseContext.cs
+++ Repository.EFCore/DatabaseContext.cs
@@ -3,37 +3,36 @@ using Repository.Core.DataModel;
 
 namespace Repository.EFCore
 {
-    public sealed class DatabaseContext : DbContext
+  public sealed class DatabaseContext : DbContext
+  {
+    public DatabaseContext(DbContextOptions options)
+      : base(options)
     {
-        public DatabaseContext(DbContextOptions options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Employer> Employers { get; set; }
-        public DbSet<Worker> Workers { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            //optionsBuilder.UseSqlite("Data Source=test.db");
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Employer>()
-                .HasMany(employer => employer.Workers)
-                .WithOne(worker => worker.Employer1);
-
-            modelBuilder.Entity<Worker>()
-              .Property(w => w.WorkerId)
-              .ValueGeneratedNever();
-
-            modelBuilder.Entity<Employer>()
-              .Property(w => w.EmployerId)
-              .ValueGeneratedNever();
     }
 
+    public DbSet<Employer> Employers { get; set; }
+    public DbSet<Worker> Workers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      //optionsBuilder.UseSqlite("Data Source=test.db");
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      modelBuilder.Entity<Employer>()
+        .HasMany(employer => employer.Workers)
+        .WithOne(worker => worker.Employer1);
+
+      modelBuilder.Entity<Worker>()
+        .Property(w => w.WorkerId)
+        .ValueGeneratedNever();
+
+      modelBuilder.Entity<Employer>()
+        .Property(w => w.EmployerId)
+        .ValueGeneratedNever();
+    }
+  }
 }
