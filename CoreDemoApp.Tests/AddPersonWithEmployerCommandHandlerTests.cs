ed8907776dc671ee26cae70e5e3f79a95ef7e208
Antti Riksman <antti.riksman@gmail.com>
2019-11-07 19:48:25 +0200

    Update AddPersonWithEmployerCommandHandlerTests.cs

--- CoreDemoApp.Tests/AddPersonWithEmployerCommandHandlerTests.cs
+++ CoreDemoApp.Tests/AddPersonWithEmployerCommandHandlerTests.cs
@@ -1,7 +1,10 @@
 using System;
 using CoreDemoApp.Application;
 using CoreDemoApp.Core.CQS;
+using CoreDemoApp.Domain;
 using Moq;
+using Repository.Core;
+using Repository.Core.DataModel;
 using Xunit;
 
 namespace CoreDemoApp.Tests
@@ -9,16 +12,33 @@ namespace CoreDemoApp.Tests
   public class AddPersonWithEmployerCommandHandlerTests
   {
     private Mock<ICommandDispatcher> _commandDispatcherMock;
+    private Mock<IUnitOfWork> _unitOfWorkMock;
 
     public AddPersonWithEmployerCommandHandlerTests()
     {
       _commandDispatcherMock = new Mock<ICommandDispatcher>();
+      _unitOfWorkMock = new Mock<IUnitOfWork>();
     }
 
     [Fact]
     public void When_Invoked_With_Invalid_Persistence_Object_Throw_Exception()
     {
       Assert.Throws<ArgumentNullException>(() => new AddPersonWithEmployerCommandHandler(null));
+    }
+
+    [Fact]
+    public void Command_Uses_Persistency_To_Add_New_Worker_Into_Database()
+    {
+      var command = new AddPersonWithEmployer(new Worker());
+      var handler = new AddPersonWithEmployerCommandHandler(_unitOfWorkMock.Object);
+
+      _unitOfWorkMock
+        .Setup(m => m.Workers.AddWorker(It.IsAny<Worker>()))
+        .Returns(() => true);
+
+      handler.Handle(command);
+
+      _unitOfWorkMock.Verify(m => m.Workers.AddWorker(It.IsAny<Worker>()), Times.Once);
     }
   }
 }
